7a638eac9f0e2811d2e035d5d402b7b48d2765df
alexandertaoadams <159815946+alexandertaoadams@users.noreply.github.com>
2025-06-18 15:43:54 +0100

    Update signature_algorithms.py

--- signature_algorithms.py
+++ signature_algorithms.py
@@ -54,7 +54,7 @@ def signature_kernel_algorithm(M, n_levels: int, order: int = 3,
 
             R_next = jax.lax.fori_loop(1, d, col_body, R_next)
             return R_next
-        
+        R_next = jax.lax.fori_loop(1, d, row_body, R_next)
         return R_next
 
 
